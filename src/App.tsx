d9eda3c46d8a321e17645f07c68b801aafa4ebb6
minhtris2005 <phamnguyenminhtri249@gmail.com>
2025-12-16 02:21:50 -0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,7 @@ const App = () => (
       <AuthProvider>
         {/* Bọc ứng dụng bằng LanguageProvider */}
         <LanguageProvider>
-          <BrowserRouter>
+          <BrowserRouter basename="drone-solutions-hub">
             <ScrollToTop />
             <AppContent />
           </BrowserRouter>
